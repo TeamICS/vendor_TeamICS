bfc891635657e8bb8c0895d30f60b10b7cb0b9d0
wjb <jaybob413@gmail.com>
2011-10-22 15:31:50 -0400

    update properties to 2.3.7 and GWK74

--- products/aosp_heroc.mk
+++ products/aosp_heroc.mk
@@ -13,7 +13,7 @@ PRODUCT_BRAND := sprint
 PRODUCT_DEVICE := heroc
 PRODUCT_MODEL := HERO200
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=GRI40 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
 
 # Extra overlay for Gallery3D orientation hack
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/heroc
@@ -22,4 +22,4 @@ PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/aosp/prelink-linu
 # Set ro.modversion
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.modversion=TeamHeroC-2.3.5-$(shell date +%0d%^b%Y-%H%M%S)
+  ro.modversion=TeamHeroC-2.3.7-$(shell date +%0d%^b%Y-%H%M%S)
