8ac0e1e3414a79bf564879d9ec8ac92baeba2d77
wjb <jaybob413@gmail.com>
2011-11-27 11:06:25 -0500

    disable hardware acceleration via build.prop

--- products/common.mk
+++ products/common.mk
@@ -18,7 +18,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    ro.config.disable_hw_accel=true
 
 # THC specific product packages
 ##PRODUCT_PACKAGES += \
